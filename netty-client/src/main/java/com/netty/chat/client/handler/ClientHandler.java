d18e53698cde0d48ebb28f45b0f901dec0bdd070
TangDynasty <tangdynasty06@163.com>
2016-08-29 17:57:24 +0800

    adjust

--- netty-client/src/main/java/com/netty/chat/client/handler/ClientHandler.java
+++ netty-client/src/main/java/com/netty/chat/client/handler/ClientHandler.java
@@ -8,25 +8,41 @@ public class ClientHandler extends ChannelInboundHandlerAdapter{
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		// TODO Auto-generated method stub
-		super.channelRead(ctx, msg);
-		ByteBuf buff = (ByteBuf)msg;
+		ByteBuf response = (ByteBuf)msg;
+		byte[] responseArr = new byte[response.readableBytes()];
+		response.readBytes(responseArr);
+		System.out.println(new String(responseArr));
+		response.release();
+		
+		
+		
+		/*ByteBuf buff = (ByteBuf)msg;
 		int b = buff.readableBytes();
-		char a = buff.readChar();
-		System.err.println("client receive msg:" + ctx.channel().id() + a);
+		byte a = buff.readByte();
+		System.err.println("client receive msg:" + ctx.channel().id() + a);*/
+		
+		//super.channelRead(ctx, msg);
 	}
 	
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		// TODO Auto-generated method stub
-		super.channelActive(ctx);
+		
+		String str = "hello,server";
+		ByteBuf buf = ctx.alloc().directBuffer(4 * str.length());
+		buf.writeBytes(str.getBytes());
+		ctx.write(buf);
+		ctx.flush();
 		System.err.println("client active send msg" + ",time:" + System.currentTimeMillis());
 		//ctx.writeAndFlush("hello server,I'm client");
+		
+		//super.channelActive(ctx);
 	}
 	
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		// TODO Auto-generated method stub
-		super.exceptionCaught(ctx, cause);
 		System.err.println("find error!");
+		super.exceptionCaught(ctx, cause);
 	}
 }
